cea9b2c27647ecbd4041c1ebf30838bb36cfdd0d
greendata <greendatacoalition@gmail.com>
2021-05-07 13:36:15 -0400

    The colors of sub_metering 2 and 3 were reversed. Fixed.

--- plot3.R
+++ plot3.R
@@ -10,20 +10,17 @@ hpcData <- fread(unzip(tempfile), sep=";", header=TRUE, na.strings="?")
 unlink(tempfile)
 
 hpcData <- subset(hpcData, Date == "1/2/2007" | Date == "2/2/2007") %>%
-    mutate(dtime = dmy_hms(paste(Date, Time, sep=" ")), wday = lubridate::wday(dtime, label=TRUE))
+    mutate(dtime = dmy_hms(paste(Date, Time, sep=" ")))
 
 
 ## create the png
 png(file="plot3.png", width=480, height=480)
 par(mar=c(4,5,3,3))
-with(hpcData, plot(dtime,Sub_metering_1, type="l", lwd=1, xlab="", ylab="Energy sub metering"))
-with(hpcData, lines(dtime, Sub_metering_2, lwd= 1, col="blue"))
-with(hpcData, lines(dtime, Sub_metering_3, lwd= 1, col="red"))
-legend(
-    "topright",
-    legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
-    col = c("black", "blue", "red"),
-    lty = 1,
-    cex = 0.8
-)
+with(hpcData, {
+    plot(dtime,Sub_metering_1, type="l", lwd=1, xlab="", ylab="Energy sub metering")
+    lines(dtime, Sub_metering_2, lwd= 1, col="red")
+    lines(dtime, Sub_metering_3, lwd= 1, col="blue")
+    legend("topright", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
+        col = c("black", "red", "blue"), lty = 1, cex = 0.8)
+})
 dev.off()
